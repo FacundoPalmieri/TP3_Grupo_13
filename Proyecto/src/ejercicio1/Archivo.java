b2aacb50d5311e1b0333bbae459dd58d1fe6e175
RSaavedraNicolas <rodrigo-saavedra@outlook.com>
2024-04-20 18:31:57 -0300

    Funciones clase Archivo

--- Proyecto/src/ejercicio1/Archivo.java
+++ Proyecto/src/ejercicio1/Archivo.java
@@ -1,16 +1,79 @@
 package ejercicio1;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Archivo {
+	
+	// Attributes
 	private String ruta;
 
-    public Archivo(String ruta) {
+	// Constructors
+	public Archivo(String ruta) {
         this.ruta = ruta;
     }
 
+	
+	// Getters y Setters
+	public String getruta() {
+		return ruta;
+	}
+
+	public void setruta(String ruta) {
+		this.ruta = ruta;
+	}
+
+	// Metodos
+	public boolean exist()
+	{
+		File file = new File(ruta); 
+		if(file.exists())
+		      return true;
+		return false;
+	}
+	
+	
+	//-------------------------------------------------------------------------------
+	// crearArchivo
+	public boolean creaArchivo()
+	{
+		FileWriter escritura;
+		try {
+			escritura = new FileWriter(ruta, true);
+			escritura.write("");
+			escritura.close();
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+			
+	}
+	
+	//-------------------------------------------------------------------------------
+	//escribeLineas
+	public void escribe_lineas(String frase) {
+		try 
+		{	
+			FileWriter entrada = new FileWriter(ruta, true);
+			BufferedWriter miBuffer = new BufferedWriter(entrada);
+			miBuffer.write(frase);
+			miBuffer.close();
+			entrada.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	//-------------------------------------------------------------------------------
+	
+	//Funcion Leer lineas
     public void lee_lineas() {
         try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
             String linea;
