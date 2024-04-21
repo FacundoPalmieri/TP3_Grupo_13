6473cf25cd015ec67e999787f0416076f4795f20
unknown <palmierifacundo@gmail.com>
2024-04-21 12:21:54 -0300

    Avances en el main

--- Proyecto/src/ejercicio1/Principal.java
+++ Proyecto/src/ejercicio1/Principal.java
@@ -1,8 +1,20 @@
 package ejercicio1;
 
+import java.util.TreeSet;
+
 public class Principal {
 
 	public static void main(String[] args) {
+	
+		Usuario usuario = new Usuario();
+		TreeSet<Usuario> listaUsuario = new TreeSet<Usuario>();
+		Archivo archivo = new Archivo("Usuarios.txt");
+		
+		if(!archivo.exist()) {
+			archivo.creaArchivo();
+		}
+		
+		archivo.lee_lineas();
 		
 
 	}
