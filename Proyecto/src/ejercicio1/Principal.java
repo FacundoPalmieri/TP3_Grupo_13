3be6d44e0c3bbafd1cd1799f0c4ca699bd9272e1
Robielys <robielys.ortiz@gmail.com>
2024-04-22 20:54:28 -0300

    prueba

--- Proyecto/src/ejercicio1/Principal.java
+++ Proyecto/src/ejercicio1/Principal.java
@@ -1,6 +1,9 @@
 package ejercicio1;
 
 import java.util.TreeSet;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Principal {
 
@@ -14,10 +17,34 @@ public class Principal {
 			archivo.creaArchivo();
 		}
 		
-		
-		TreeSet<Usuario> listaUsuarios = new TreeSet<>();
-		
 
-	}
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo.getruta()))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                if (!linea.isEmpty()) { // Verificar si la línea está vacía
+                    String[] partes = linea.split("-");
+                    if (partes.length == 2) { // Verificar si se dividió en dos partes correctamente
+                        int id = Integer.parseInt(partes[0].trim());
+                        String nombreUsuario = partes[1].trim();
+                        
+                        try {
+                            if (Usuario.verificarUsuarioInvalido(nombreUsuario)) {
+                                Usuario nuevoUsuario = new Usuario(id, nombreUsuario);
+                                listaUsuario.add(nuevoUsuario);
+                            }
+                        } catch (UsuarioInvalidoException e) {
+                            System.out.println("El usuario \"" + nombreUsuario + "\" no es válido.");
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("No se pudo leer el archivo.");
+        }
 
+        System.out.println("Usuarios válidos:");
+        for (Usuario u : listaUsuario) {
+            System.out.println(u);
+        }
+    }
 }
